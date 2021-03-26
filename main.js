4baa7632a827d208f47345514c0d79e83e64f814
Artur-123-123 <arturolejnik462@gmail.com>
2021-03-26 15:22:17 +0100

    26.03.2021

---
+++ main.js
@@ -0,0 +1,14 @@
+function menu() {
+    const pizza = document.getElementById('pizza').value
+    console.log(pizza)
+}
+
+function formularz() {
+    const imie = document.getElementById('imie').value
+    console.log(imie)
+}
+
+function formularz2() {
+    const naz = document.getElementById('nazwisko').value
+    console.log(naz)
+}
